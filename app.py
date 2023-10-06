37b054dd2975b0455faf2e3ec4da4e3df28f2f49
Rafael Angelo Pucut <rpucut@gmail.com>
2023-10-05 21:19:02 -0600

    Create app.py
    created python file for website, added streamlit as package through pip

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+# https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Who is Rafael?", page_icon=":thinking_face:", layout="wide")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Hi, I am Rafael :wave:")
+    st.title("A Software Developer From Canada")
+    st.write("I am passionate about learning different tech stacks and being able to apply them in functional applications I work on.")
+    st.write("[Learn More >](https://www.rpucut.tech/)")
